61805e2b906acb45645976da30c74fde7dd1aea5
KarimovFirdavs <115251742+KarimovFirdavs@users.noreply.github.com>
2022-10-08 17:29:23 +0500

    Add files via upload

---
+++ JavaScript/js/script.js
@@ -0,0 +1,6 @@
+let name = prompt('whats your name', '')
+console.log('hello ' + name);
+let age = prompt('how old are you ', '');
+console.log(' ok ' + age + ' years old ');
+let education = prompt('where do study');
+console.log(education + ' ok ');
---
+++ JavaScript/js2/script.js
@@ -0,0 +1,3 @@
+let age = prompt('son kiriting');
+let age2 = prompt('ikkinchi sonni kiriting');
+console.log(+ age + +age2);
